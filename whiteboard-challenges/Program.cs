a8d30fa2010c316a919fb4a803cce9478804ff1e
nooralbonne <24042306@student.ltuc.com>
2024-06-06 00:50:15 +0300

    Add project files.

---
+++ whiteboard-challenges/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace whiteboard_challenges
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
