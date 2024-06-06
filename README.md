34efe57afe55741b23459724c6727b6cce950323
nooralbonne <24042306@student.ltuc.com>
2024-06-06 09:19:58 +0300

    Add Readme.md file for description and submit the assigment

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # whiteboard-challenges
+
+### Whiteboard Images
+
+### Challenge A: Array Reversal
+
+![Array Reversal Whiteboard](ArrayReversalWhiteboard.png)
+Reverse an array without using built-in methods in C#.
+
+### Challenge B: Most Frequent Number
+
+![Most Frequent Number Whiteboard](/MostFrequentNumber.png)
+Find the number that appears the most times in an array, returning the first number if there are no duplicates or the first found in case of a tie.
--- whiteboard-challenges/Program.cs
+++ whiteboard-challenges/Program.cs
@@ -1,11 +1,35 @@
-﻿namespace whiteboard_challenges
+﻿using System;
+public class GFG
 {
-    internal class Program
+    public static int mostFrequent(int[] arr, int n)
     {
-        static void Main(string[] args)
+        int maxcount = 0;
+        int element_having_max_freq = 0;
+        for (int i = 0; i < n; i++)
         {
-            Console.WriteLine("Hello, World!");
+            int count = 0;
+            for (int j = 0; j < n; j++)
+            {
+                if (arr[i] == arr[j])
+                {
+                    count++;
+                }
+            }
 
+            if (count > maxcount)
+            {
+                maxcount = count;
+                element_having_max_freq = arr[i];
+            }
         }
+
+        return element_having_max_freq;
+    }
+
+    public static void Main(String[] args)
+    {
+        int[] arr = { 1, 1, 2, 2, 3, 3, 3, 1, 1, 5, 5, 6, 7, 8, 2, 1, 1 };
+        int n = arr.Length;
+        Console.Write(mostFrequent(arr, n));
     }
 }
