e40aec625ad1397fc5f2e965617af7ff48514866
nooralbonne <24042306@student.ltuc.com>
2024-07-01 18:59:00 +0300

    Add project files.

---
+++ Test-whiteboard-challenges/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Test_whiteboard_challenges
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ whiteboard-challenges/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace whiteboard_challenges
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+        }
+    }
+}
