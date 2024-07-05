307aa70d293fc2c5619834de6cc802c6deb88728
nooralbonne <24042306@student.ltuc.com>
2024-07-05 23:53:34 +0300

    add ReverseWord#

---
+++ Challenges/Reverse-Words/Reverse-Words/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Reverse_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "csharp is programming language";
+            string reversed = StringManipulation.ReverseWords(input);
+            Console.WriteLine(reversed);  // Output: "language programming is csharp"
+        }
+    }
+
+    public class StringManipulation
+    {
+        public static string ReverseWords(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return input;
+            }
+
+            string[] words = input.Split(' ');
+            Array.Reverse(words);
+            return string.Join(' ', words);
+        }
+    }
+}
